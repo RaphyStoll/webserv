b33e71d875852a976e6b6c6575be9b13cb4b2c74
RaphyStoll <raphael.ferreira@ik.me>
2026-01-26 18:02:08 +0100

    remove:: template

--- include/BootStrap.hpp
+++ include/BootStrap.hpp
@@ -5,34 +5,31 @@
 # include <iostream>
 
 # include "../lib/LIBFTPP/include/libftpp.hpp"
+# include "Config.hpp"
 
 namespace webserv {
 
 	/**
 	 * @brief Classe Bootstrap
 	 * 
-	 * Elle prend un type TConfig
-	 * Cela permet de changer la structure de la config (Map, Vector, List, ...
-	 * sans avoir à modifier ce header
+	 * Elle prend un type NetworkConfig
 	 */
-	template <typename TConfig>
 	class BootStrap {
 		public:
-			BootStrap(const TConfig& par_config) : _logger("BootStrap"), _config(par_config) {}
+			BootStrap(const NetworkConfig& par_config);
 			~BootStrap() {}
 
 			void start();
+			const std::vector<int>& getListenSockets() const;
 
 		private:
 			libftpp::debug::DebugLogger _logger;
-			TConfig _config;
+			NetworkConfig _config;
 			std::vector<int> _listen_sockets;
 			void _setup_sockets();
 	};
 
 	int create_listener_socket(int port);
 }
-
-#include "BootStrap.tpp"
 
 #endif

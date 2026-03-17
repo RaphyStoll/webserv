58164960027cd0a2cedaa570e5c8742d50c5b2d0
Frac25 <sylvain.dubois25@hotmail.fr>
2026-03-17 19:04:02 +0000

    corr_site_2_3

---
+++ www/site3/api/php/hello.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+// Simple PHP CGI Script for Demo
+// Ensure this script is executable (chmod +x hello.php)
+
+$name = "Guest";
+if (isset($_GET['name']) && !empty($_GET['name'])) {
+    $name = htmlspecialchars($_GET['name']);
+}
+
+// CGI Response headers
+echo "Content-Type: text/plain\r\n";
+echo "X-Powered-By: Webserv-CGI\r\n";
+echo "\r\n"; // End of headers
+
+// Body
+echo "Hello, " . $name . "!\n";
+echo "Welcome to the Webserv CGI Executable Environment.\n";
+echo "Server Software: " . $_SERVER['SERVER_SOFTWARE'] . "\n";
+echo "Current Time: " . date('Y-m-d H:i:s') . "\n";
+?>
